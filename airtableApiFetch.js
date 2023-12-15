6dab35cd7b57351c3224900d839daf97084b7a29
ByrdBassPC <byrdbass@gmail.com>
2023-12-14 18:16:49 -0600

    added resources for TIME app in Softr, and started working on Airtable API Fetch

---
+++ airtableApiFetch.js
@@ -0,0 +1,29 @@
+require('dotenv').config(); 
+
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.airtableApiKey
+
+});
+var base = Airtable.base('apps7roRhnziLR2ou');
+
+base('Projects_Active').select({
+    // Selecting the first 3 records in Active_Projects:
+    // maxRecords: 3,
+    view: "Active_Projects"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('ClientAbbrev_Project_Link'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
--- airtableBudget-Formulas.js
+++ airtableBudget-Formulas.js
@@ -3,4 +3,4 @@ IF(LEN(Project_Cost_Rollup_String) >= 4,
 LEFT({Project_Cost_Rollup_String}, 
     LEN({Project_Cost_Rollup_String}) - 3) 
     & "," & 
-RIGHT({Project_Cost_Rollup_String}, 3), {Project_Cost_Rollup_String})
+RIGHT({Project_Cost_Rollup_String}, 3), {Project_Cost_Rollup_String})c
